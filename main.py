9d7e3b3bb3f519213e24bcc2e432095695fdcc68
jsbailey <jason.bailey@imperial.ac.uk>
2024-10-20 18:48:37 +0100

    add testing

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World! Again</p>"
+
+
+def process_query(query: str):
+    if query == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+
+    if query == "asteroids":
+        return "Unknown"
+
+    return "Query not known"
